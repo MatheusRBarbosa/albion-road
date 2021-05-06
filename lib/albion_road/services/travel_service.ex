a5d9249df616d3e956060de5deca9cec03fdab91
Matheus Barbosa <matheuskleber@live.com>
2021-05-06 18:13:26 -0300

    fix(travel): match city correctly

--- lib/albion_road/services/travel_service.ex
+++ lib/albion_road/services/travel_service.ex
@@ -37,8 +37,9 @@ defmodule AlbionRoad.Services.TravelService do
           buy_price_max_date: item["buy_price_max_date"]
         }
       end)
-      |> calc_profit()
+      |> calc_profit(cities)
       |> Enum.filter(& !is_nil(&1))
+      |> Enum.filter(&(&1.buy_in == cities.from))
       |> Enum.sort(&(&1.avg_profit >= &2.avg_profit))
       |> Enum.take(25)
 
@@ -56,25 +57,37 @@ defmodule AlbionRoad.Services.TravelService do
     city["name"]
   end
 
-  defp calc_profit(items) do
-    Enum.map(items, fn %PricesStruct{} = i -> find_pair_and_calc(items, i) end)
+  defp calc_profit(items, %TravelStruct{} = cities) do
+    Enum.map(items, fn %PricesStruct{} = i -> find_pair_and_calc(items, i, cities) end)
   end
 
-  defp find_pair_and_calc(items, %PricesStruct{} = item) do
+  defp find_pair_and_calc(items, %PricesStruct{} = item, %TravelStruct{} = cities) do
     pair_finded = Enum.find(items, fn %PricesStruct{} = i -> i.item_id == item.item_id end)
     if pair_finded != nil do
-      avg_buy = (item.buy_price_min + item.buy_price_max)/2
-      avg_sell = (pair_finded.sell_price_min + pair_finded.sell_price_max)/2
+      match_item_city(item, pair_finded, String.downcase(item.city), cities)
+    end
+  end
 
-      if avg_buy != 0 and avg_sell != 0 do
-        avg_profit = avg_sell - avg_buy
-        %ItemsAvgStruct{
-          item_id: item.item_id,
-          avg_buy_price_from_city: avg_buy,
-          avg_sell_price_to_city: avg_sell,
-          avg_profit: avg_profit
-        }
-      end
+  defp match_item_city(%PricesStruct{} = item, %PricesStruct{} = pair, item_city,  %TravelStruct{} = cities)
+    when item_city == cities.from,
+    do: parse_item_avg(item, pair, cities.from, cities.to)
+
+  defp match_item_city(%PricesStruct{} = item, %PricesStruct{} = pair, _item_city,  %TravelStruct{} = cities),
+    do: parse_item_avg(pair, item, cities.to, cities.from)
+
+  defp parse_item_avg(%PricesStruct{} = item_from, %PricesStruct{} = pair_to, from_city, to_city) do
+    avg_buy = (item_from.buy_price_min + item_from.buy_price_max)/2
+    avg_sell = (pair_to.sell_price_min + pair_to.sell_price_max)/2
+    if avg_buy != 0 and avg_sell != 0 do
+    avg_profit = avg_sell - avg_buy
+      %ItemsAvgStruct{
+        item_id: item_from.item_id,
+        avg_buy_price_from_city: avg_buy,
+        avg_sell_price_to_city: avg_sell,
+        avg_profit: avg_profit,
+        buy_in: from_city,
+        sell_in: to_city
+      }
     end
   end
 
--- lib/albion_road/structs/items_avg_struct.ex
+++ lib/albion_road/structs/items_avg_struct.ex
@@ -3,5 +3,7 @@ defmodule AlbionRoad.Structs.ItemsAvgStruct do
   defstruct item_id: "",
             avg_buy_price_from_city: 0,
             avg_sell_price_to_city: 0,
-            avg_profit: 0
+            avg_profit: 0,
+            buy_in: "",
+            sell_in: ""
 end
